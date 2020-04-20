50134d91f74af8a1dd9707bdb7d2ea3e84d625e1
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-20 20:11:12 +0800

    12th iteration with pug

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
 
+
+const port = process.env.PORT || 3000;
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (require, response) => {
-  response.sendFile(path.join(`${__dirname}/views/index.html`));
+  response.render('index', { list: ['a', 'b'] });
+  // response.sendFile(path.join(`${__dirname}/views/index.html`));
 });
 
-app.listen(3000, () => {
-  debug(`listening to a port ${chalk.yellowBright(' 3000')}`);
+app.listen(port, () => {
+  debug(`listening on port ${chalk.yellowBright(port)}`);
 });
