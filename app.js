b875ba12488c3f8900aaf2d50fcf534f4e5820cd
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-20 16:21:21 +0800

    install eslin support ./node_modules/.bin/eslint app.js --fix

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
-var app = express();
-var debug = require('debug')('app');
-var chalk = require('chalk');
-var morgan = require('morgan');
-var path = require('path');
+
+const app = express();
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname,'/public')));
-app.use('/css',express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
-app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
-app.get('/',function(require,response){
-response.sendFile(path.join(__dirname + '/views/index.html'));
+app.get('/', (require, response) => {
+  response.sendFile(path.join(`${__dirname}/views/index.html`));
 });
 
-app.listen(3000, function(){
-    debug(`listening to a port ${chalk.yellowBright(' 3000')}`);
+app.listen(3000, () => {
+  debug(`listening to a port ${chalk.yellowBright(' 3000')}`);
 });
