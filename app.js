d4e8a79f2be96a6780cf97b9e83687f0f34a7c2b
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-20 14:04:57 +0800

    fifth iteration adding morgan to spit out some message during browsing

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 var app = express();
 var debug = require('debug')('app');
 var chalk = require('chalk');
+var morgan = require('morgan');
+
+app.use(morgan('tiny'));
 
 app.get('/',function(require,response){
 response.send('Hello World');
