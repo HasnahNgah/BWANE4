5671787647d07ede5968b0f3967686d89eb49e82
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-20 15:47:56 +0800

    9th iteration serving all locally

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var path = require('path');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public')));
+app.use('/css',express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
+app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
+app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')));
+
 app.get('/',function(require,response){
 response.sendFile(path.join(__dirname + '/views/index.html'));
 });
