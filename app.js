5822279374d500410b607c7db3b9c74bad2d853c
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-20 13:43:22 +0800

    adding chalk module for coloring important properties

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 var app = express();
+var chalk = require('chalk');
 
 app.get('/',function(require,response){
 response.send('Hello World');
 });
 
 app.listen(3000, function(){
-    console.log('listening to a port');
+    console.log('listening to a port' + chalk.redBright(' 3000'));
 });
