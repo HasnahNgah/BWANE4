08757c0c830a7bbb2522fd9aeaa14012aed227ec
Hasnah Ngah <hasnah@Hasnahs-MacBook-Air.local>
2020-04-21 20:29:05 +0800

    20th iteration using mssql npm mpdule

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const debug = require('debug')('app');
 const chalk = require('chalk');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 
 const port = process.env.PORT || 3000;
 
+const config = {
+  user: 'azlan',
+  password: 'passw0rdMUKAH',
+  server: 'azlanlibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'PSLibrary',
+};
+sql.connect(config).catch((err) => { debug(err); });
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
